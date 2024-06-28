5688f4ba1d3eaebac6793ee6f816f1a1c717ca4c
katamaiah <katam.gowd@gmail.com>
2024-06-28 11:26:05 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain.llms.openai import OpenAI
 
-st.title('🦜🔗 Katam's first LLM App')
+st.title('🦜🔗 Katam first LLM App')
 
 openai_api_key = st.sidebar.text_input('OpenAI API Key', type='password')
 
